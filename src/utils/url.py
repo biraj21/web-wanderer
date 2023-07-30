32831998108b2c14d7fa5f7006a1bec7c34824cb
Biraj <biraj.pub@gmail.com>
2023-07-30 13:57:23 +0530

    fix: use regex to replace multiple / with _

--- src/utils/url.py
+++ src/utils/url.py
@@ -1,3 +1,5 @@
+import re
+
 from urllib.parse import urlparse, urljoin
 
 
@@ -26,29 +28,32 @@ def get_cleaned_url(url):
     if not all([parsed_url.scheme, parsed_url.netloc]):
         return None
 
-    return f"{parsed_url.scheme}://{parsed_url.netloc}{parsed_url.path}"
+    return f"{parsed_url.scheme}://{parsed_url.netloc}{re.sub(r'/+', '/', parsed_url.path)}"
 
 
 def get_filename(url, skip_base_url=False):
+    if type(url) != str:
+        return None
+
     parsed_url = urlparse(url)
 
     if not all([parsed_url.scheme, parsed_url.netloc]):
         return None
-
-    if skip_base_url and not parsed_url.path:
-        raise Exception("url needs path when skip_base_url=True")
 
     if skip_base_url:
         filename = parsed_url.path
     else:
         filename = f"{parsed_url.netloc}{parsed_url.path}"
 
-    filename = filename.replace("/", "_")
+    filename = re.sub(r"/+", "_", filename)
 
     if filename.startswith("_"):
         filename = filename[1:]
 
     if filename.endswith("_"):
         filename = filename[:-1]
+
+    if len(filename) == 0:
+        return "index"
 
     return filename
